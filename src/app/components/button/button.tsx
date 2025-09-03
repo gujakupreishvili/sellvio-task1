e9e8d19cb8fc5e5eb1a1b7fdb9813f28833f57fe
guja kupreishvili <gujakupreishvili4@gmail.com>
2025-09-04 02:26:01 +0400

    feat:props

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Image, { StaticImageData } from "next/image";
 type Props = {
  text?: string
- icon?: React.ElementType
+ icon?: React.ElementType | undefined
  Img?:StaticImageData
  className: string; 
  pTagStyle?:string
+ iconStyle?:string
 }
 
-export default function Button({ text, icon: Icon, Img , className ,pTagStyle}: Props) {
+export default function Button({ text, icon: Icon, Img , className ,pTagStyle,iconStyle }: Props) {
   return (
    <button className={`${className}`}>
-    {Icon && <Icon size={20} />}
+    {Icon && <Icon className={`${iconStyle}`} />}
     {Img && <Image src={Img} alt="button icon" width={20} height={20} />}
     {text && <p className={`${pTagStyle}`}>{text}</p>}
    </button>
