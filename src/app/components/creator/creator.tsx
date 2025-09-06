d17cd727e6af0082ae74e5cfdd50d685f026c3fd
guja kupreishvili <gujakupreishvili4@gmail.com>
2025-09-06 10:44:46 +0400

    style:add height

--- src/app/components/creator/creator.tsx
+++ src/app/components/creator/creator.tsx
@@ -6,7 +6,7 @@ type Props = {
   description: string;
   Img: StaticImageData;
   isSelected?: boolean;
-  onClick?: (title: string) => void;
+  onClick?: (title: string) => void; 
 };
 
 export default function Creator({ title, description, Img, isSelected = false, onClick }: Props) {
@@ -16,16 +16,15 @@ export default function Creator({ title, description, Img, isSelected = false, o
       className={`
         flex items-start border-[1px] border-[#0000001F] rounded-[8px] 
         shadow-[0px_0px_9.8px_0px_#00000014] w-[48%] px-[30px] pt-[16px] pb-[34px] gap-[17px]
-        cursor-pointer transition-colors
-        ${isSelected ? "bg-[#0866FF20]" : "bg-[#0866FF05]"}
+        cursor-pointer transition-colors  xl:h-[118px]       ${isSelected ? "bg-[#0866FF20]" : "bg-[#0866FF05]"}
       `}
     >
-      <div className="w-[48px] h-[48px] bg-[#0866FF0F] rounded-[8px] flex items-center justify-center">
+      <div className="min-w-[48px] h-[48px] bg-[#0866FF0F] rounded-[8px] flex items-center justify-center">
         <Image src={Img} alt="" />
       </div>
       <div className="flex flex-col gap-[10px]">
         <p className="text-[18px] font-sans-georgian font-bold text-black">{title}</p>
-        <p className="text-[#000000AD] text-[14.35px] font-sans-georgian font-normal">{description}</p>
+        <p className="text-[#000000AD] text-[14.35px] font-sans-georgian font-normal ">{description}</p>
       </div>
     </div>
   );
